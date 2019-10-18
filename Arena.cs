4d19e0daa586b9df42b2faabce5601047cd9577a
mikhailryn <mdryndin@gmail.com>
2019-10-18 19:30:50 +0300

    Mod_Game_Arena

---
+++ Arena.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Diagnostics;
+using static System.Console;
+
+namespace Arena
+{
+    public class Arena
+    {
+        private Warrior warrior1;
+        private Warrior warrior2;
+        private Dice dice;
+        private enum Direction
+        {
+            Up,
+            Down,
+            Right,
+            Left
+        }
+
+        public Arena(Warrior warrior1, Warrior warrior2, Dice dice)
+        {
+            this.warrior1 = warrior1;
+            this.warrior2 = warrior2;
+            this.dice = dice;
+        }
+
+        //static 
+        private Direction ReadMovement(Direction movement)
+        {
+            if (KeyAvailable)
+            {
+                var key = ReadKey(true).Key;
+
+                if (key == ConsoleKey.UpArrow)
+                {
+                    movement = Direction.Up;
+                }
+                else if (key == ConsoleKey.DownArrow)
+                {
+                    movement = Direction.Down;
+                }
+                else if (key == ConsoleKey.LeftArrow)
+                {
+                    movement = Direction.Left;
+                }
+                else if (key == ConsoleKey.RightArrow)
+                {
+                    movement = Direction.Right;
+                }
+            }
+
+            return movement;
+        }
+        private void Render()
+        {
+            Console.Clear();
+            Console.WriteLine("-------------- Ринг -------------- \n");
+            Console.WriteLine("Здоровье бойцов: \n");
+            Console.WriteLine("{0} {1}", warrior1, warrior1.HealthBar());
+            Console.WriteLine("{0} {1}", warrior2, warrior2.HealthBar());
+        }
+
+        private void PrintMessage(string message)
+        {
+            Console.WriteLine(message);
+            Thread.Sleep(500);
+        }
+
+        public void Fight()
+        {
+            // The original order
+            Warrior w1 = warrior1;
+            Warrior w2 = warrior2;
+            Console.WriteLine("Добро пожаловать в ринг!");
+            Console.WriteLine("Сегодня {0} будет драться с {1}! \n", warrior1, warrior2);
+            // swapping the warriors
+            bool warrior2Starts = (dice.Roll() <= dice.GetSidesCount() / 2);
+            if (warrior2Starts)
+            {
+                w1 = warrior2;
+                w2 = warrior1;
+            }
+            Console.WriteLine("Боксер {0} начинает! \nПриступим к бою...", w1);
+            Console.ReadKey();
+
+            var currentMovement = Direction.Right;
+
+            while (w1.Alive() && w2.Alive())
+            {
+                PrintMessage("Выберите направление!");
+
+                var sw = Stopwatch.StartNew();
+                while (sw.ElapsedMilliseconds <= 500)
+                {
+                    currentMovement = ReadMovement(currentMovement);
+                }
+
+                PrintMessage($"Направление {currentMovement}");
+
+                switch (currentMovement)
+                {
+                    case Direction.Left:
+                        if (w1.Alive())
+                        {
+                            w1.Attack(w2);
+                            Render();
+                            PrintMessage(w1.GetLastMessage()); // message about the attack
+                            PrintMessage(w2.GetLastMessage()); // message about the defense
+                        }
+                        break;
+                    case Direction.Right:
+                        if (w2.Alive())
+                        {
+                            w2.Attack(w1);
+                            Render();
+                            PrintMessage(w2.GetLastMessage()); // message about the attack
+                            PrintMessage(w1.GetLastMessage()); // message about the defense
+                        }
+                        break;
+                }
+
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ Warrior.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Arena
+{
+
+   public class Warrior
+    {
+
+        private string name;
+
+        private int health;
+   
+        private int maxHealth;
+  
+        private int damage;
+       
+        private int defense;
+   
+        private Dice dice;
+     
+        private string message;
+
+ 
+        /// <param name="name">Warrior's name</param>
+        /// <param name="health">Health in HP</param>
+        /// <param name="damage">Damage in HP</param>
+        /// <param name="defense">Defense in HP</param>
+        /// <param name="dice">Dice instance</param>
+        public Warrior(string name, int health, int damage, int defense, Dice dice)
+        {
+            this.name = name;
+            this.health = health;
+            this.maxHealth = health;
+            this.damage = damage;
+            this.defense = defense;
+            this.dice = dice;
+        }
+
+        public override string ToString()
+        {
+            return name;
+        }
+
+
+        public bool Alive()
+        {
+            return (health > 0);
+        }
+
+        public string HealthBar()
+        {
+            string s = "[";
+            int total = 20;
+            double count = Math.Round(((double)health / maxHealth) * total);
+            if ((count == 0) && (Alive()))
+                count = 1;
+            for (int i = 0; i < count; i++)
+                s += "#";
+            s = s.PadRight(total + 1);
+            s += "]";
+            return s;
+        }
+
+        /// <param name="enemy">Enemy instance</param>
+        public void Attack(Warrior enemy)
+        {
+            int hit = damage + dice.Roll();
+            SetMessage(String.Format("{0} attacks with {1} hp hit", name, hit));
+            enemy.Defend(hit);
+        }
+
+        /// <param name="hit">Hit power in HP</param>
+        public void Defend(int hit)
+        {
+            int injury = hit - (defense + dice.Roll());
+            if (injury > 0) // Injured
+            {
+                health -= injury;
+                message = String.Format("{0} got an injury of {1} hp", name, injury);
+                if (health <= 0)
+                {
+                    health = 0;
+                    message += " and died";
+                }
+            }
+            else // No damage taken
+                message = String.Format("{0} parried a hit", name);
+            SetMessage(message);
+        }
+
+        /// <param name="message">User message about last action</param>
+        private void SetMessage(string message)
+        {
+            this.message = message;
+        }
+
+        public string GetLastMessage()
+        {
+            return message;
+        }
+
+    }
+}
